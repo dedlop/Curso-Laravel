8bde8781c4b601b6e35206ab0c2121e6c159da48
dedlop <104914772+dedlop@users.noreply.github.com>
2024-04-05 11:34:44 -0300

    Adicionado Checkbox e Date

--- hdcevents/resources/views/events/show.blade.php
+++ hdcevents/resources/views/events/show.blade.php
@@ -12,9 +12,15 @@
             <div id="info-container" class="col-md-6">
                 <h1>{{ $event->title }}</h1>
                 <p class="event-city"><ion-icon name="pin"></ion-icon> {{ $event->city }} </p>
-                <p class="events-paticipants"><ion-icon name="people-outline"></ion-icon> X Participantes</p>
+                <p class="events-paticipants"><ion-icon name="people"></ion-icon> X Participantes</p>
                 <p class="event-owner"><ion-icon name="star-outline"></ion-icon>Dono do Evento</p>
                 <a href="#" class="btn btn-primary" id="event-submit">Confirmar Presença</a>
+                <h3>O evento conta com:</h3>
+                <ul id="items-list">
+                @foreach($event->items as $item)
+                    <li><ion-icon name="play"></ion-icon> <span>{{ $item }}</span></li>
+                @endforeach
+                </ul>
             </div>
             <div class="col-md-12" id="description-container">
                 <h3>Sobre o evento:</h3>
